88e8a5bf6d85dc03d11b1f3924bb9e8d9a502d27
James B <jamesaburnell@gmail.com>
2015-10-24 00:58:53 -0700

    click event for lightbox

---
+++ js/mss.js
@@ -0,0 +1,16 @@
+
+//close lightbox
+// $('.lightbox').click(function(){
+// 		$('.lightbox').css('display', 'none');
+// 		console.log('working');
+// 	});
+
+
+//open lightbox
+$('#maxonProduct1').click(function(){
+		$('#lightbox1').css('display', 'block');
+		console.log('working');
+	});
+
+
+
